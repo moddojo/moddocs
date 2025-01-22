f387e3249df1f44f65c02973086db023f13c4a77
Mod Dojo <196010969+moddojo@users.noreply.github.com>
2025-01-22 17:27:11 +1000

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
   url: 'https://moddojo.netlify.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/moddocs/',
+  baseUrl: 'https://github.com/moddojo/moddocs',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
